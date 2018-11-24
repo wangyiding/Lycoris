9a175819b045b0437baba1956d5de45480194fcf
eding <eding@DESKTOP-B0RFLSG>
2018-11-24 18:32:29 +0800

    增加json支持

--- src/org/edingsoft/config/Webinitializer.java
+++ src/org/edingsoft/config/Webinitializer.java
@@ -3,8 +3,6 @@ package org.edingsoft.config;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
-
-import org.edingsoft.config.web.Thymeleaf;
 import org.edingsoft.config.web.WebConfig;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
